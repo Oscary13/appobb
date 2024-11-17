22208cb34a39e27bf430fd499ed9059a98193d42
Oscary13 <oscary_1320114020@uptecamac.edu.mx>
2024-11-16 22:52:35 -0600

    CREATE - El inicio de todo :3

---
+++ src/main/java/com/obbapp/mx/appobb/AppobbApplication.java
@@ -0,0 +1,13 @@
+package com.obbapp.mx.appobb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppobbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppobbApplication.class, args);
+	}
+
+}
